3df5678c8b2749e5e3ee81bcbf91e78224e74f75
NateWizardry <183080080+NateWizardry@users.noreply.github.com>
2024-10-31 23:12:48 +0530

    Work in progress

--- app/src/main/java/org/example/UI.java
+++ app/src/main/java/org/example/UI.java
@@ -1,8 +1,19 @@
 package org.example;
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class UI {
 
@@ -70,11 +81,28 @@ public class UI {
         // Action listeners for buttons
         submitButton.addActionListener((ActionEvent e) -> {
             String selectedMood = "";
-            if (mainCharacterButton.isSelected()) selectedMood = "Main Character Energy";
-            else if (petDogButton.isSelected()) selectedMood = "Might pet a random dog.";
-            else if (mehButton.isSelected()) selectedMood = "Meh";
-            else if (potatoButton.isSelected()) selectedMood = "Current mood: potato";
-            else if (helpButton.isSelected()) selectedMood = "Send help and snacks";
+            int selectedMoodValue;
+            if (mainCharacterButton.isSelected()) {
+                selectedMood = "Main Character Energy";
+                selectedMoodValue = 5;
+                }
+            else if (petDogButton.isSelected()){ 
+                selectedMood = "Might pet a random dog.";
+                selectedMoodValue = 4;
+            }
+            else if (mehButton.isSelected()) {
+                selectedMood = "Meh";
+                selectedMoodValue = 3;
+                }
+            else if (potatoButton.isSelected()) {
+                selectedMood = "Current mood: potato";
+                selectedMoodValue = 2;
+            }
+            else if (helpButton.isSelected()) {
+                selectedMood = "Send help and snacks";
+                selectedMoodValue = 1;
+            }
+
             
             if (!selectedMood.isEmpty()) {
                 JOptionPane.showMessageDialog(frame, "You selected: " + selectedMood);
